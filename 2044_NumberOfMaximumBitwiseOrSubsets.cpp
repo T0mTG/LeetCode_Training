4d4f78cf13ddfe94986978c1f9f8be8b030d6011
T0mTG <dogiaminh0612@gmail.com>
2024-10-18 12:05:37 -0500

    2044

--- 2044_NumberOfMaximumBitwiseOrSubsets.cpp
+++ 2044_NumberOfMaximumBitwiseOrSubsets.cpp
@@ -8,6 +8,7 @@ void gen(int i,int maxi,int tmp,vector<int>& nums)
     if(i==nums.size()) return;
     for(i=i+1;i<nums.size();i++)
     {
+        // more backtracking with more nums added to the subsets
         gen(i,maxi,tmp|nums[i],nums);
     }
     return;
@@ -16,13 +17,14 @@ int countMaxOrSubsets(vector<int>& nums) {
     int maxi=nums[0],n=nums.size();
     for(int i=1;i<n;i++)
     {
-        //the maximum bitwise-OR subsets is the bitwise-OR of the whole array 
+        // the maximum bitwise-OR subsets is the bitwise-OR of the whole array 
         maxi=maxi|nums[i];
         //cout<<nums[i]<<" "<<maxi<<endl;
     }    
     //cout<<maxi<<endl;
     for(int i=0;i<n;i++)
     {
+        // backtracking by starting with each individual element of the array
         gen(i,maxi,nums[i],nums);
     }
     //cout<<res<<endl;   
