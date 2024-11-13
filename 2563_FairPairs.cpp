ad6f33bad5e09dda746b8002222d40880674a476
T0mTG <dogiaminh0612@gmail.com>
2024-11-13 15:10:58 -0600

    2563

---
+++ 2563_FairPairs.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long uppe(vector<int>& nums,long long l, long long target){
+    long long n=nums.size();
+    long long r=n-1,res=0;
+    while(l<r)
+    {
+        //cout<<l<<": "<<nums[l]<<" || "<<r<<": "<<nums[r]<<endl;
+        if(nums[l]+ nums[r]<=target){ 
+            //if the sum between the two ends is smaller than the target, than for every number between l and r
+            //combines with nums[l] will also be smaller target, this is true because we sorted the array
+            res+=r-l;
+            //increase l to check the next number
+            l++; 
+        }
+        else r--;
+    }
+    return res;
+}
+long long lowe(vector<int>& nums,long long l, long long target){
+    long long n=nums.size();
+    long long r=n-1,res=0;
+    while(l<r)
+    {
+        if(nums[l]+ nums[r]<target){ 
+            res+=r-l;
+            l++;
+        }
+        else r--;
+    }
+    return res;
+}
+long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    cout<<"///"<<endl;
+    long long n=nums.size();
+    sort(nums.begin(), nums.end());
+    // for(auto i:nums) cout<<i<<" ";
+    // cout<<endl;
+
+    //uppe is basically a two-pointer function that finds the nums of pairs that are <= upper,
+    //including pairs with sum < lower
+    
+    //lowe is the same as uppe, but it finds the nums of pairs that are smaller than lower,
+    //therefore, we can eliminate the pairs with sums < lower
+
+    // hence res=uppe - lowe;
+    long long res=uppe(nums,0,upper)-lowe(nums,0,lower);
+
+    //visualization
+    //                                        lower           upper
+    //Sums of pairs:             ///////////////////////////////////////
+    //Number of pairs from uppe: ///////////////////////////////|
+    //Number of pairs from lowe: ///////////////|
+    //Res:                                      |///////////////|
+    
+    cout<<uppe(nums,0,upper)<<" || "<<lowe(nums,0,lower)<<endl;
+    cout<<res;
+    return res;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int>a;
+    int n,x,l,u;
+    cin>>n>>l>>u;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>x;
+        a.push_back(x);
+    }
+    countFairPairs(a,l,u);
+}
+
+/*
+6 3 6
+0 1 7 4 4 5
+
+5 11 11
+1 7 9 2 5
+*/
