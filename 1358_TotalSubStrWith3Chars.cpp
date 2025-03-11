eeb54eee0ca1f2c988570a5052d77f53033edd7e
T0mTG <dogiaminh0612@gmail.com>
2025-03-11 15:55:16 -0500

    1358

--- 1358_TotalSubStrWith3Chars.cpp
+++ 1358_TotalSubStrWith3Chars.cpp
@@ -2,6 +2,9 @@
 #define ll long long
 using namespace std;
 int numberOfSubstrings(string s) {
+    //the idea for this problem is to check for substring with at least 1 occurence of a,b,c and then add the rest of the
+    //chars behind it to get the sum
+    //for example: if you can find abc...(the rest of the string), then whether it's abca,abccc,abcbcbcbc,... it would be eligible 
     map<char,int>d;
     int res=0;
     d['a']=0;
@@ -11,11 +14,14 @@ int numberOfSubstrings(string s) {
     d[s[0]]++;
     while(r<s.size()&&l<=r){
         if(!(d['a']&&d['b']&&d['c'])){
+            //if there's not enough a,b,c, extend r to fill in
             r++;
             d[s[r]]++;
         }
         else{
-            cout<<l<<" "<<r<<endl   ;
+            //if there's enough a,b,c, add the number of remaining chars to res
+            //for example:if you can find something acb, then acba,acbad,... are also eligible
+            //cout<<l<<" "<<r<<endl   ;
             res+=s.size()-r;
             d[s[l]]--;
             l++;
